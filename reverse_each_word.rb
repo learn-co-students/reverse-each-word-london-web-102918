c1f31b062cf55db7585737ae21905f03f34835e5
Paul Clark <clark.paul123@gmail.com>
2018-10-19 15:54:07 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,16 @@
+=begin
+
 def reverse_each_word(sentence)
     arr_split = sentence.split(" ")
     reversed = []
     arr_split.collect do |word| reversed << word.reverse
   end
+  reversed.join(" ")
+end
+
+=end
+
+def reverse_each_word(sentence)
+  reversed = sentence.split(" ").collect {|word| word.reverse}
   reversed.join(" ")
 end
